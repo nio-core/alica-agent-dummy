6df2125b88fee78752e726a88a1db54b4afcd4a6
Sven Starcke <sst97@gmx.net>
2020-05-17 17:31:24 +0200

    Implementation based on a single publisher and subscriber using all message types

--- include/agent.h
+++ include/agent.h
@@ -17,19 +17,10 @@ public:
 private:
     void *zmq_context;
     MonitoredPublisher *publisher;
-    MonitoredSubscriber *allocationAuthoritySubscriber;
-    MonitoredSubscriber *engineInfoSubscriber;
-    MonitoredSubscriber *entryPointRobotsSubscriber;
-    MonitoredSubscriber *planTreeInfoSubscriber;
-    MonitoredSubscriber *roleSwitchSubscriber;
-    MonitoredSubscriber *solverResultSubscriber;
-    MonitoredSubscriber *syncDataSubscriber;
-    MonitoredSubscriber *syncReadySubscriber;
-    MonitoredSubscriber *syncTalkSubscriber;
+    MonitoredSubscriber *subscriber;
 
     void sendAllocationAuthorityInfo() const;
     void sendEngineInfo() const;
-    void sendEntryPointRobots() const;
     void sendPlanTreeInfo() const;
     void sendRoleSwitch() const;
     void sendSolverResult() const;
--- src/agent.cpp
+++ src/agent.cpp
@@ -10,49 +10,9 @@
 #include <alica_msgs/SyncReady.capnp.h>
 #include <alica_msgs/SyncTalk.capnp.h>
 
-void handleAllocationAuthority(capnp::FlatArrayMessageReader &message)
+void handleMessage(capnp::FlatArrayMessageReader &message)
 {
-    std::cout << "Got message on [ALLOCATION_AUTHORITY]: " << message.getRoot<alica_msgs::AllocationAuthorityInfo>().toString().flatten().cStr() << std::endl;
-}
-
-void hanldeEngineInfo(capnp::FlatArrayMessageReader &message)
-{
-    std::cout << "Got message on [ENGINE_INFO]: " << message.getRoot<capnzero::String>().getString().cStr() << std::endl;
-}
-
-void handleEntryPointRobots(capnp::FlatArrayMessageReader &message)
-{
-    std::cout << "Got message on [ENTRYPOINT_ROBOTS]: " << message.getRoot<alica_msgs::EntrypointRobots>().toString().flatten().cStr() << std::endl;
-}
-
-void handlePlanTreeInfo(capnp::FlatArrayMessageReader &message)
-{
-    std::cout << "Got message on [PLAN_TREE_INFO]: " << message.getRoot<alica_msgs::PlanTreeInfo>().toString().flatten().cStr() << std::endl;
-}
-
-void handleRoleSwitch(capnp::FlatArrayMessageReader &message)
-{
-    std::cout << "Got message on [ROLE_SWITCH]: " << message.getRoot<alica_msgs::RoleSwitch>().toString().flatten().cStr() << std::endl;
-}
-
-void handleSolverResult(capnp::FlatArrayMessageReader &message)
-{
-    std::cout << "Got message on [SOLVER_RESULT]: " << message.getRoot<alica_msgs::SolverResult>().toString().flatten().cStr() << std::endl;
-}
-
-void handleSyncData(capnp::FlatArrayMessageReader &message)
-{
-    std::cout << "Got message on [SYNC_DATA]: " << message.getRoot<alica_msgs::SyncData>().toString().flatten().cStr() << std::endl;
-}
-
-void handleSyncReady(capnp::FlatArrayMessageReader &message)
-{
-    std::cout << "Got message on [SYNC_READY]: " << message.getRoot<alica_msgs::SyncReady>().toString().flatten().cStr() << std::endl;
-}
-
-void handleSyncTalk(capnp::FlatArrayMessageReader &message)
-{
-    std::cout << "Got message on [SYNC_TALK]: " << message.getRoot<alica_msgs::SyncTalk>().toString().flatten().cStr() << std::endl;
+    std::cout << "Got message " << message.getRoot<capnzero::String>().getString().cStr() << std::endl;
 }
 
 Agent::Agent(int id, int localPort) : zmq_context(zmq_ctx_new())
@@ -60,85 +20,36 @@ Agent::Agent(int id, int localPort) : zmq_context(zmq_ctx_new())
     publisher = new MonitoredPublisher(std::to_string(id) + "/0", zmq_context, capnzero::Protocol::UDP);
     std::cout << "SETUP: Starting publisher " << std::to_string(id) << "/0" << std::endl;
 
-    allocationAuthoritySubscriber = new MonitoredSubscriber(std::to_string(id) + "/1", zmq_context, capnzero::Protocol::UDP);
-    allocationAuthoritySubscriber->addAddress("*:" + std::to_string(localPort));
-    allocationAuthoritySubscriber->setTopic("ALLOC_AUTH");
-    allocationAuthoritySubscriber->subscribe(handleAllocationAuthority);
-    std::cout << "SETUP: Starting Allocation Authority Subscriber " << std::to_string(id) << "/1"
-              << " on Topic [ALLOC_AUTH]" << std::endl;
+    subscriber = new MonitoredSubscriber(std::to_string(id) + "/1", zmq_context, capnzero::Protocol::UDP);
+    std::cout << "SETUP: Starting subscriber " << std::to_string(id) << "/1" << std::endl;
+    subscriber->addAddress("*:" + std::to_string(localPort));
 
-    engineInfoSubscriber = new MonitoredSubscriber(std::to_string(id) + "/2", zmq_context, capnzero::Protocol::UDP);
-    engineInfoSubscriber->addAddress("*:" + std::to_string(localPort));
-    engineInfoSubscriber->setTopic("ENGINE_INFO");
-    engineInfoSubscriber->subscribe(hanldeEngineInfo);
-    std::cout << "SETUP: Engine Info Subscriber " << std::to_string(id) << "/2"
-              << " on Topic [ENGINE_INFO]" << std::endl;
+    subscriber->setTopic("ALLOC_AUTH");
+    std::cout << "SETUP: Subscribing to topic [ALLOC_AUTH]" << std::endl;
 
-    entryPointRobotsSubscriber = new MonitoredSubscriber(std::to_string(id) + "/3", zmq_context, capnzero::Protocol::UDP);
-    entryPointRobotsSubscriber->addAddress("*:" + std::to_string(localPort));
-    entryPointRobotsSubscriber->setTopic("EP_ROBOTS");
-    entryPointRobotsSubscriber->subscribe(handleEntryPointRobots);
-    std::cout << "SETUP: Starting Entry Point Robots Subscirber " << std::to_string(id) << "/3"
-              << " on Topic [EP_ROBOTS]" << std::endl;
+    subscriber->setTopic("ENGINE_INFO");
+    std::cout << "SETUP: Subscribing to topic [ENGINE_INFO]" << std::endl;
 
-    planTreeInfoSubscriber = new MonitoredSubscriber(std::to_string(id) + "/4", zmq_context, capnzero::Protocol::UDP);
-    planTreeInfoSubscriber->addAddress("*:" + std::to_string(localPort));
-    planTreeInfoSubscriber->setTopic("PT_INFO");
-    planTreeInfoSubscriber->subscribe(handlePlanTreeInfo);
-    std::cout << "SETUP: Starting Plan Tree Info Subscriber " << std::to_string(id) << "/4"
-              << " on Topic [PT_INFO]" << std::endl;
+    subscriber->setTopic("PT_INFO");
+    std::cout << "SETUP: Subscribing to topic [PT_INFO]" << std::endl;
 
-    roleSwitchSubscriber = new MonitoredSubscriber(std::to_string(id) + "/5", zmq_context, capnzero::Protocol::UDP);
-    roleSwitchSubscriber->addAddress("*:" + std::to_string(localPort));
-    roleSwitchSubscriber->setTopic("ROLE_SWITCH");
-    roleSwitchSubscriber->subscribe(handleRoleSwitch);
-    std::cout << "SETUP: Starting Role Switch Subscriber " << std::to_string(id) << "/5"
-              << " on Topic [ROLE_SWITCH]" << std::endl;
+    subscriber->setTopic("ROLE_SWITCH");
+    std::cout << "SETUP: Subscribing to topic [ROLE_SWITCH]" << std::endl;
 
-    solverResultSubscriber = new MonitoredSubscriber(std::to_string(id) + "/6", zmq_context, capnzero::Protocol::UDP);
-    solverResultSubscriber->addAddress("*:" + std::to_string(localPort));
-    solverResultSubscriber->setTopic("SOLVER_RESULT");
-    solverResultSubscriber->subscribe(handleSolverResult);
-    std::cout << "SETUP: Starting Solver Result Subscriber " << std::to_string(id) << "/6"
-              << " on Topic [SOLVER_RESULT]" << std::endl;
+    subscriber->setTopic("SOLVER_RESULT");
+    std::cout << "SETUP: Subscribing to topic [SOLVER_RESULT]" << std::endl;
 
-    syncDataSubscriber = new MonitoredSubscriber(std::to_string(id) + "/7", zmq_context, capnzero::Protocol::UDP);
-    syncDataSubscriber->addAddress("*:" + std::to_string(localPort));
-    syncDataSubscriber->setTopic("SYNC_DATA");
-    syncDataSubscriber->subscribe(handleSyncReady);
-    std::cout << "SETUP: Starting Sync Data Subscriber " << std::to_string(id) << "/7"
-              << " on Topic [SYNC_DATA]" << std::endl;
+    subscriber->setTopic("SYNC_READY");
+    std::cout << "SETUP: Subscribing to topic [SYNC_READY]" << std::endl;
 
-    syncReadySubscriber = new MonitoredSubscriber(std::to_string(id) + "/8", zmq_context, capnzero::Protocol::UDP);
-    syncReadySubscriber->addAddress("*:" + std::to_string(localPort));
-    syncReadySubscriber->setTopic("SYNC_READY");
-    syncReadySubscriber->subscribe(handleSyncReady);
-    std::cout << "SETUP: Starting Sync Ready Subscriber " << std::to_string(id) << "/8"
-              << " on Topic [SYNC_READY]" << std::endl;
+    subscriber->setTopic("SYNC_TALK");
+    std::cout << "SETUP: Subscribing to topic [SYNC_TALK]" << std::endl;
 
-    syncTalkSubscriber = new MonitoredSubscriber(std::to_string(id) + "/9", zmq_context, capnzero::Protocol::UDP);
-    syncTalkSubscriber->addAddress("*:" + std::to_string(localPort));
-    syncTalkSubscriber->setTopic("SYNC_TALK");
-    syncTalkSubscriber->subscribe(handleSyncTalk);
-    std::cout << "SETUP: Starting Sync Talk Subscriber " << std::to_string(id) << "/9"
-              << " on Topic [SYNC_TALK]" << std::endl;
+    subscriber->subscribe(handleMessage);
 }
 
 Agent::~Agent()
 {
-    /*zmq_ctx_term(zmq_context);
-    zmq_ctx_destroy(zmq_context);
-
-    delete publisher;
-    delete allocationAuthoritySubscriber;
-    delete engineInfoSubscriber;
-    delete entryPointRobotsSubscriber;
-    delete planTreeInfoSubscriber;
-    delete roleSwitchSubscriber;
-    delete solverResultSubscriber;
-    delete syncDataSubscriber;
-    delete syncReadySubscriber;
-    delete syncTalkSubscriber;*/
 }
 
 void Agent::attachListener(const std::string &listenerAddress, const std::string &listenerTopic)
@@ -160,15 +71,11 @@ void Agent::run(const std::string &destination)
 
     sendEngineInfo();
 
-    sendEntryPointRobots();
-
     sendPlanTreeInfo();
 
     sendRoleSwitch();
 
     sendSolverResult();
-
-    sendSyncData();
 
     sendSyncReady();
 
@@ -177,7 +84,30 @@ void Agent::run(const std::string &destination)
 
 void Agent::sendAllocationAuthorityInfo() const
 {
-    std::cout << "LOG: Sending Allocation Authority Information" << std::endl;
+    capnp::MallocMessageBuilder message;
+
+    alica_msgs::AllocationAuthorityInfo::Builder allocationAuthority = message.initRoot<alica_msgs::AllocationAuthorityInfo>();
+
+    capnzero::ID::Builder senderID = allocationAuthority.initSenderId();
+    senderID.setType(0);
+    senderID.initValue(0);
+
+    capnp::List<alica_msgs::EntrypointRobots, capnp::Kind::STRUCT>::Builder entrypointRobots = allocationAuthority.initEntrypointRobots(1);
+    alica_msgs::EntrypointRobots::Builder robot = entrypointRobots[0];
+    robot.setEntrypoint(0);
+    robot.initRobots(0);
+
+    allocationAuthority.setPlanId(1);
+    allocationAuthority.setParentState(2);
+    allocationAuthority.setPlanType(1);
+    capnzero::ID::Builder authority = allocationAuthority.initAuthority();
+    authority.setType(0);
+    authority.initValue(0);
+
+    const std::string msg = message.getRoot<alica_msgs::AllocationAuthorityInfo>().toString().flatten().cStr();
+
+    std::cout << "LOG: Sending Allocation Authority Information to [ALLOC_AUTH]: " << msg << std::endl;
+    publisher->send(msg, "ALLOC_AUTH");
 }
 
 void Agent::sendEngineInfo() const
@@ -189,7 +119,7 @@ void Agent::sendEngineInfo() const
     capnp::List<capnzero::ID>::Builder agentsWithMe = engineInfo.initAgentIdsWithMe(1);
     capnzero::ID::Builder agentId = agentsWithMe[0];
     agentId.setType(0);
-    agentId.setValue(capnp::Data::Reader());
+    agentId.initValue(0);
 
     engineInfo.setCurrentPlan("MyPlan");
     engineInfo.setCurrentRole("MyRole");
@@ -207,37 +137,104 @@ void Agent::sendEngineInfo() const
     publisher->send(msg, "ENGINE_INFO");
 }
 
-void Agent::sendEntryPointRobots() const
-{
-    std::cout << "LOG: Sending Entry Point Robots" << std::endl;
-}
-
 void Agent::sendPlanTreeInfo() const
 {
-    std::cout << "LOG: Sending Plan Tree Info" << std::endl;
+    capnp::MallocMessageBuilder message;
+
+    alica_msgs::PlanTreeInfo::Builder planTreeInfo = message.initRoot<alica_msgs::PlanTreeInfo>();
+    capnzero::ID::Builder senderId = planTreeInfo.initSenderId();
+    senderId.setType(0);
+    senderId.initValue(0);
+
+    capnp::List<int64_t>::Builder stateIds = planTreeInfo.initStateIds(1);
+    stateIds.set(0, 0);
+
+    capnp::List<int64_t>::Builder eps = planTreeInfo.initSucceededEps(2);
+    eps.set(0, 0);
+    eps.set(1, 1);
+
+    const std::string msg = message.getRoot<alica_msgs::PlanTreeInfo>().toString().flatten().cStr();
+
+    std::cout << "LOG: Sending Plan Tree Info to [PT_INFO]: " << msg << std::endl;
+
+    publisher->send(msg, "PT_INFO");
 }
 
 void Agent::sendRoleSwitch() const
 {
-    std::cout << "LOG: Sending Role Switch" << std::endl;
+    capnp::MallocMessageBuilder message;
+
+    alica_msgs::RoleSwitch::Builder roleSwitch = message.initRoot<alica_msgs::RoleSwitch>();
+    capnzero::ID::Builder senderId = roleSwitch.initSenderId();
+    senderId.setType(0);
+    senderId.initValue(0);
+
+    roleSwitch.setRoleId(0);
+
+    const std::string msg = message.getRoot<alica_msgs::RoleSwitch>().toString().flatten().cStr();
+
+    std::cout << "LOG: Sending Role Switch to [ROLE_SWITCH]: " << msg << std::endl;
+    publisher->send(msg, "ROLE_SWITCH");
 }
 
 void Agent::sendSolverResult() const
 {
-    std::cout << "LOG: Sending Solver Result" << std::endl;
-}
+    capnp::MallocMessageBuilder message;
 
-void Agent::sendSyncData() const
-{
-    std::cout << "LOG: Sending Sync Data" << std::endl;
+    alica_msgs::SolverResult::Builder solverResult = message.initRoot<alica_msgs::SolverResult>();
+    capnzero::ID::Builder senderId = solverResult.initSenderId();
+    senderId.setType(0);
+    senderId.initValue(0);
+
+    capnp::List<alica_msgs::SolverVar>::Builder variables = solverResult.initVars(1);
+    alica_msgs::SolverVar::Builder var = variables[0];
+    var.setId(0);
+    capnp::List<uint8_t>::Builder values = var.initValue(1);
+    values.set(0, 1);
+
+    const std::string msg = message.getRoot<alica_msgs::SolverResult>().toString().flatten().cStr();
+
+    std::cout << "LOG: Sending Solver Result to [SOLVER_RESULT]: " << msg << std::endl;
+    publisher->send(msg, "SOLVER_RESULT");
 }
 
 void Agent::sendSyncReady() const
 {
-    std::cout << "LOG: Sending Sync Ready" << std::endl;
+    capnp::MallocMessageBuilder message;
+
+    alica_msgs::SyncReady::Builder syncReady = message.initRoot<alica_msgs::SyncReady>();
+    capnzero::ID::Builder senderId = syncReady.initSenderId();
+    senderId.setType(0);
+    senderId.initValue(0);
+
+    syncReady.setSynchronisationId(0);
+
+    const std::string msg = message.getRoot<alica_msgs::SyncReady>().toString().flatten().cStr();
+
+    std::cout << "LOG: Sending Sync Ready to [SYNC_READY]: " << msg << std::endl;
+    publisher->send(msg, "SYNC_READY");
 }
 
 void Agent::sendSyncTalk() const
 {
-    std::cout << "LOG: Sending Sync Talk" << std::endl;
+    capnp::MallocMessageBuilder message;
+    alica_msgs::SyncTalk::Builder syncTalk = message.initRoot<alica_msgs::SyncTalk>();
+    capnzero::ID::Builder senderId = syncTalk.initSenderId();
+    senderId.setType(0);
+    senderId.initValue(0);
+
+    capnp::List<alica_msgs::SyncData>::Builder syncData = syncTalk.initSyncData(1);
+    alica_msgs::SyncData::Builder data = syncData[0];
+    capnzero::ID::Builder robotId = data.initRobotId();
+    robotId.setType(1);
+    robotId.initValue(0);
+
+    data.setTransitionId(0);
+    data.setTransitionHolds(false);
+    data.setAck(true);
+
+    const std::string msg = message.getRoot<alica_msgs::SyncTalk>().toString().flatten().cStr();
+
+    std::cout << "LOG: Sending Sync Talk to [SYNC_TALK]: " << msg << std::endl;
+    publisher->send(msg, "SYNC_TALK");
 }
